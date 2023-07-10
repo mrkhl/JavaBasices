f4decb2efe9a946bd4fffea5add8460a60fef846
mrkhl <48253036+mrkhl@users.noreply.github.com>
2023-07-10 09:09:42 +0400

    Project added

---
+++ src/main/java/om/gov/omantax/Main.java
@@ -0,0 +1,9 @@
+package om.gov.omantax;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("second Hello World");
+        System.out.println("jkhjkhgs");
+    }
+}

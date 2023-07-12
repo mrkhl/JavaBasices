38684f11679f3be5c511c69de5d712f37322b8d8
mrkhl <48253036+mrkhl@users.noreply.github.com>
2023-07-12 11:06:30 +0400

    collection and map

--- src/main/java/om/gov/omantax/Main.java
+++ src/main/java/om/gov/omantax/Main.java
@@ -7,24 +7,46 @@ import om.gov.omantax.entity.User;
 import om.gov.omantax.entity2.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import static om.gov.omantax.entity.OrderStatus.PLACED;
 
 public class Main {
+
     public static void main(String[] args) {
-        Rectangle rec = new Rectangle(10.0,10.0);
-        Triangle tri = new Triangle(5.0,5.0);
-        Square sqr = new Square(10.0);
-        Circle cir = new Circle(2.5);
 
-        Shape[] shapes1 = new Shape[4];
-        shapes1[0] = rec;
-        shapes1[1] = tri;
-        shapes1[2] = cir;
-        shapes1[3] = sqr;
+        //Collection and Map >>>>>>>>>>
 
-        double sum = sumOfAres(shapes1);
-        System.out.println(sum);
+
+/*        List<Integer> list = new ArrayList<>();
+        list.add(111);
+        list.add(222);
+        list.add(333);
+        System.out.println(list);*/
+
+
+        //Collection and Map >>>>>>>>>>
+
+
+
+
+        //polymorphism,interference,Generics>>>>>>>
+//        Rectangle rec = new Rectangle(10.0,10.0);
+//        Triangle tri = new Triangle(5.0,5.0);
+//        Square sqr = new Square(10.0);
+//        Circle cir = new Circle(2.5);
+//
+//        Shape[] shapes1 = new Shape[4];
+//        shapes1[0] = rec;
+//        shapes1[1] = tri;
+//        shapes1[2] = cir;
+//        shapes1[3] = sqr;
+//
+//        double sum = sumOfAres(shapes1);
+//        System.out.println(sum);
+    //polymorphism,interference,Generics<<<<<
 
 //
 //        Product pro = new Product(12,"hammd",1.0);
@@ -37,11 +59,13 @@ public class Main {
 
 
     }
-    public static double sumOfAres(Shape[] shapes){
-        double sum =0;
-        for (Shape sh: shapes){
-            sum+=sh.calcArea();
-        }
-        return sum;
-    }
+    //polymorphism,interference,Generics>>>>>>>
+//    public static double sumOfAres(Shape[] shapes){
+//        double sum =0;
+//        for (Shape sh: shapes){
+//            sum+=sh.calcArea();
+//        }
+//        return sum;
+//    }
+    //polymorphism,interference,Generics<<<<<<<
 }
